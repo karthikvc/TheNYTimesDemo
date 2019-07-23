87209dd2d768944509da3ddd8e807063b762d69f
karthikeyan <karthikvc@gmail.com>
2019-07-23 17:50:29 +0530

    Test coverage

--- TheNYTimesDemoTests/TheNYTimesDemoTests.swift
+++ TheNYTimesDemoTests/TheNYTimesDemoTests.swift
@@ -11,17 +11,58 @@ import XCTest
 
 class TheNYTimesDemoTests: XCTestCase {
 
+    var storyboard: UIStoryboard?
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.storyboard = UIStoryboard(name: "Main",
+                                       bundle: Bundle.main)
+        
+        XCTAssertNotNil(self.storyboard)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
+    
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testMasterView(){
+        
+        let masterViewController = self.storyboard?.instantiateViewController(withIdentifier: "MasterViewcontroller")
+        
+        XCTAssertNotNil(masterViewController)
+        XCTAssertTrue(masterViewController is SectionsViewController)
+        
+    }
+    
+    func testFetchData()  {
+        
+        let expectation = self.expectation(description:  "JSON Response Received")
+        Networking().performFetch(with: APIService.getSectionWith(topic: .ALL_Section, period: 1), completion: {(Result) in
+            
+            XCTAssertEqual(Result.status, "OK", "Data Fetch Successefully")
+            //XCTAssertEqual(Result.status, "Not OK", "Data Fetch Failed")
+            
+            expectation.fulfill()
+            
+            
+        },
+                                  completionWith: { (Error) in
+                                    XCTFail("NetWork error")
+                                    
+        }
+            
+        )
+        
+        self.waitForExpectations(timeout: 30) { error in
+            XCTAssertNil(error)
+        }
+        
     }
 
     func testPerformanceExample() {
--- TheNYTimesDemoUITests/TheNYTimesDemoUITests.swift
+++ TheNYTimesDemoUITests/TheNYTimesDemoUITests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class TheNYTimesDemoUITests: XCTestCase {
 
+    var app = XCUIApplication()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -19,6 +21,9 @@ class TheNYTimesDemoUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
+        app = XCUIApplication()
+        
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -29,6 +34,47 @@ class TheNYTimesDemoUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    
+    func testTableInteraction() {
+        // Assert that we are displaying the tableview
+        app.launch()
+        
+        sleep(15)
+        let cells = app.tables.cells
+        //snapshot("App Screenshot Launch with indicator")
+        
+        // Get an array of cells
+        if cells.count > 0 {
+            //snapshot("after load")
+            let count: Int = (cells.count - 1)
+            
+            let promise = expectation(description: "Wait for table cells")
+            
+            for i in stride(from: 0, to: count , by: 1) {
+                // Grab the first cell and verify that it exists and tap it
+                let tableCell = cells.element(boundBy: i)
+                
+                XCTAssertTrue(tableCell.exists, "The \(i) cell is in place on the table")
+                
+                // Does this actually take us to the next screen
+                tableCell.tap()
+                
+                if i == (count - 1) {
+                    //snapshot("detail View")
+                    promise.fulfill()
+                }
+                
+                // Back
+                app.navigationBars.buttons.element(boundBy: 0).tap()
+            }
+            
+            waitForExpectations(timeout: 50, handler: nil)
+            XCTAssertTrue(true, "Finished validating the table cells")
+        } else {
+            XCTAssert(false, "Was not able to find any table cells")
+        }
     }
 
 }
